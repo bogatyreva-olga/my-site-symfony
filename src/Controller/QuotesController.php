73a67a3514bb0f603941e303b5c3e6d53e555e13
o.bogatyreva <bogatireva.o.v@gmail.com>
2023-11-07 20:17:16 +0300

    add debugger and profiler

--- src/Controller/QuotesController.php
+++ src/Controller/QuotesController.php
@@ -1,9 +1,11 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,34 +14,34 @@ class QuotesController extends AbstractController
 
     private const QUOTES = [
         [
-            "id"=> 1,
-            "text"=> "Скалолазание - моя свобода. Моя жизнь в вертикальном мире ",
-            "author"=> "Линн Хилл"
+            "id" => 1,
+            "text" => "Скалолазание - моя свобода. Моя жизнь в вертикальном мире ",
+            "author" => "Линн Хилл"
         ],
         [
-            "id"=> 2,
-            "text"=> "Мозг- самая важная мышца в скалолазании",
-            "author"=> "Вольфганг Гюллих"
+            "id" => 2,
+            "text" => "Мозг- самая важная мышца в скалолазании",
+            "author" => "Вольфганг Гюллих"
         ],
         [
-            "id"=> 3,
-            "text"=> "Я никогда не тренируюсь на самом деле. Я просто всегда был скалолазом",
-            "author"=> "Крис Шарма"
+            "id" => 3,
+            "text" => "Я никогда не тренируюсь на самом деле. Я просто всегда был скалолазом",
+            "author" => "Крис Шарма"
         ],
         [
-            "id"=> 4,
-            "text"=> "Никогда я не исследовал жизнь так интенсивно в ее красоте, как когда я свободно висел на кончиках двух пальцев над глубокой впадиной",
-            "author"=> "Вольфганг Гюллих"
+            "id" => 4,
+            "text" => "Никогда я не исследовал жизнь так интенсивно в ее красоте, как когда я свободно висел на кончиках двух пальцев над глубокой впадиной",
+            "author" => "Вольфганг Гюллих"
         ],
         [
-            "id"=> 5,
-            "text"=> "Есть два типа альпинистов: те, кто поднимается, потому что их сердце поет, когда они в горах, и все остальные",
-            "author"=> "Алекс Лоу"
+            "id" => 5,
+            "text" => "Есть два типа альпинистов: те, кто поднимается, потому что их сердце поет, когда они в горах, и все остальные",
+            "author" => "Алекс Лоу"
         ],
         [
-            "id"=> 6,
-            "text"=> "Горы - это не стадионы, на которых я удовлетворяю свои амбиции, это соборы, где я исповедую свою религию",
-            "author"=> "Анатолий Букреев"
+            "id" => 6,
+            "text" => "Горы - это не стадионы, на которых я удовлетворяю свои амбиции, это соборы, где я исповедую свою религию",
+            "author" => "Анатолий Букреев"
         ],
     ];
 
@@ -50,11 +52,23 @@ class QuotesController extends AbstractController
     {
         return $this->render('quotes/quotes.html.twig');
     }
+
     /**
      * @Route("/random-quotes")
      */
-    public function getRandomQuotes(): JsonResponse
+    public function getRandomQuotes(Request $request): JsonResponse
     {
-        return $this->json(self::QUOTES[0]);
+        $parameterQuote = (int)$request->query->get('excludeId');
+
+        $quotes = self::QUOTES;
+
+        if ($parameterQuote > 0) {
+            $quotes = array_filter($quotes, function ($el) use ($parameterQuote) {
+                return $el['id'] !== $parameterQuote;
+            });
+        }
+        $randomIndexQuote = rand(0, count($quotes));
+
+        return $this->json($quotes[$randomIndexQuote]);
     }
 }
