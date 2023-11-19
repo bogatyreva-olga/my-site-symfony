bf65098ef725beec508097b6301052a72a9bc684
o.bogatyreva <bogatireva.o.v@gmail.com>
2023-11-19 18:53:27 +0300

    add categories to twig

--- src/Controller/FeedbackController.php
+++ src/Controller/FeedbackController.php
@@ -15,6 +15,23 @@ class FeedbackController extends AbstractController
      */
     public function feedbackMessagesRender(): Response
     {
-        return $this->render('feedback-message/feedback-message.html.twig');
+        return $this->render('feedback-message/feedback-message.html.twig', [
+            'feedbackMessages' => $this->getFeedbackMessagesByCategoryId(),
+            'categories' => $this->getFeedbackCategories(),
+        ]);
+    }
+
+    private function getFeedbackMessagesByCategoryId()
+    {
+
+    }
+
+    private function getFeedbackCategories(): array
+    {
+        return [
+            ["id" => 1, "name" => "Магазин"],
+            ["id" => 2, "name" => "Сайт"],
+            ["id" => 3, "name" => "Расписание"],
+        ];
     }
 }
