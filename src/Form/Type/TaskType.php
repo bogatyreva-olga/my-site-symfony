b3647cc6ceb33aa04df553a8c782186b91bc8001
o.bogatyreva <bogatireva.o.v@gmail.com>
2024-01-21 22:18:39 +0300

    add constraints to validator

--- src/Form/Type/TaskType.php
+++ src/Form/Type/TaskType.php
@@ -8,7 +8,9 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class TaskType extends AbstractType
 {
@@ -21,6 +23,28 @@ class TaskType extends AbstractType
             ->add('password', TextType::class, [
                 'constraints' => [
                     new NotBlank(),
+                    new Length([
+                        'min' => 6,
+                        'max' => 16,
+                        'minMessage' => 'Your password must be at least 6 characters long',
+                        'maxMessage' => 'Your password cannot be longer than 16 characters',
+                    ]),
+                    new Regex([
+                        'pattern' => '/\d+/',
+                        'match' => true,
+                        'message' => 'Your password must have at least one number']),
+                    new Regex([
+                        'pattern' => '/[A-Z]+/',
+                        'match' => true,
+                        'message' => 'Your password must have at least one uppercase letter']),
+                    new Regex([
+                        'pattern' => '/[a-z]+/',
+                        'match' => true,
+                        'message' => 'Your password must have at least one lowercase letter']),
+                    new Regex([
+                        'pattern' => '/^[a-zA-Z0-9!?]+$/',
+                        'match' => true,
+                        'message' => 'You are entering invalid symbols']),
                 ],
             ])
             ->add('save', SubmitType::class)
