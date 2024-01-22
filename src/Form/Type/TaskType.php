5c667f1f4e3df82f6d5bb024296f88a8cfd2b2c1
o.bogatyreva <bogatireva.o.v@gmail.com>
2024-01-22 14:00:38 +0300

    add constraints to email

--- src/Form/Type/TaskType.php
+++ src/Form/Type/TaskType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Regex;
@@ -18,7 +19,10 @@ class TaskType extends AbstractType
     {
         $builder
             ->add('email', TextType::class, [
-                'constraints' => new NotBlank(),
+                'constraints' => [
+                    new NotBlank(),
+                    new Email(),
+                    ],
             ])
             ->add('password', TextType::class, [
                 'constraints' => [
